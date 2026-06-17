0cead99d393d451fb9d8fc8a48edf54d1a754841
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2026-06-17 15:00:47 +0100

    Boom

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np 
+from sklearn.multioutput import MultiOutputRegressor 
+from sklearn.neural_network import MLPRegressor 
+import cv2 
+
+def main(): 
+    print("Hello World")
+
+
---
+++ model.py
@@ -0,0 +1,6 @@
+import numpy as np 
+from sklearn.multioutput import MultiOutputRegressor 
+from sklearn.neural_network import MLPRegressor 
+import cv2 
+
+
