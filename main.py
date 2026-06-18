a4a3acaa85b063f2b038828c775f38e30efe8385
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2026-06-18 10:35:58 +0100

    Basic model works

--- main.py
+++ main.py
@@ -7,29 +7,79 @@ from camera import *
 from shared import SharedFrame 
 from model import * 
 import tkinter as tk 
+import pyautogui
+
+pyautogui.FAILSAFE = False 
+
+# modifiable parameters 
+display_height = 1200 
+display_width = 1920
 
 frame = SharedFrame() 
 
-# launch camera 
 camera_thread = threading.Thread(target=camera_loop, args=(frame,))
 camera_thread.start() 
+
+def gui_calibration(): 
+    get_calibration_data(frame) 
 
 def gui_train(): 
     user = input("File name of the dataset: \n") 
     train("./dataset/" + user + ".csv")
 
+def smooth_predictions(predictions, current_mouse_pos, display_width, display_height): 
+    alpha = 0.1 
+    pred_x = predictions[0] * display_width
+    pred_y = predictions[1] * display_height
+
+    x = current_mouse_pos[0] * alpha + pred_x * (1-alpha)
+    y = current_mouse_pos[1] * alpha + pred_y * (1-alpha)
+    return int(x),int(y)
+
+
+def gui_playground(): 
+    user = input("File name of the model: \n") 
+    model = load_model(user)
+    cv2.namedWindow("Playground", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(
+        "Playground", 
+        cv2.WND_PROP_FULLSCREEN, 
+        cv2.WINDOW_FULLSCREEN
+    )
+
+    while True: 
+        predictions = predict(frame, model)
+        current_mouse_pos = pyautogui.position()
+        if predictions is not None: 
+            x,y = smooth_predictions(predictions, current_mouse_pos, display_width, display_height)
+            if x < 0: 
+                x = 0 
+            elif x > display_width: 
+                x = display_width 
+            if y < 0: 
+                y = 0 
+            elif y > display_height: 
+                y = display_height
+            pyautogui.moveTo(x, y)
+
+        key = cv2.waitKey(1)
+        if key == ord("q"): 
+            break 
+
+
+
 root = tk.Tk() 
 root.title("Eye Tracker Control Panel")
-root.geometry("300x150")
+root.geometry("300x300")
 
 calibration_button = tk.Button(
     root,
-    text="Calibration", 
+    text="Launch Calibration", 
     command=lambda: get_calibration_data(frame),
     width=20, 
     height=2
-)
-calibration_button.pack()
+).pack()
+
 
 train_button = tk.Button(
     root, 
@@ -37,7 +87,14 @@ train_button = tk.Button(
     command=gui_train, 
     width=20, 
     height=2
-)
-train_button.pack()
+).pack()
+
+playground_button = tk.Button(
+    root, 
+    text="Launch Playground", 
+    command=gui_playground, 
+    width=20, 
+    height=2
+).pack()
 
 root.mainloop()
--- model.py
+++ model.py
@@ -11,6 +11,7 @@ from mediapipe.tasks.python import vision
 import pyautogui 
 from util import unique_filename
 import pandas as pd 
+import pickle 
 
 # modifiable parameters - change to suit your computer 
 display_width = 1920 
@@ -28,29 +29,44 @@ detector = vision.FaceLandmarker.create_from_options(options)
 
 # landmark numbers from https://storage.googleapis.com/mediapipe-assets/documentation/mediapipe_face_landmark_fullsize.png
 # these landmarks give a good sized box around the eye for the model to learn from
-# right now the landmarks are only immediately surrounding the scelera and including the box around the iris.  
+# right now the landmarks are only immediately surrounding the scelera and including the box around the iris, as well as a cross (+) on the face for gaze direction info
 right_eye_landmarks = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7, 468, 471, 469, 470, 472]
-nose_landmarks = [4, 5, 195]
+nose_landmarks = [4, 5, 195, 197, 6, 168, 8, 9, 151, 10]
+eyeline_landmarks = [193, 417, 189, 413, 190, 414, 35, 143, 34, 127, 265, 372, 264, 356]
 left_eye_landmarks = [463, 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398, 476, 475, 474, 477, 473]
 
-def get_positions(shared_frame: SharedFrame, landmarks, landmark_indexes, output_target, draw=True, anchor=4):
+
+class Model: 
+    def __init__(self, regressor: MultiOutputRegressor, scaler: StandardScaler): 
+        self.regressor = regressor 
+        self.scaler = scaler 
+    
+    def predict(self, shared_frame: SharedFrame): 
+        if shared_frame.frame is not None: 
+            features = feature_extraction(shared_frame) 
+            scaled_features = self.scaler.transform([features])
+            predictions = self.regressor.predict(scaled_features)
+            return predictions[0]
+
+
+def get_positions(shared_frame: SharedFrame, landmarks, landmark_indexes, output_target, draw=False, anchor=4):
     # this function also assumes lock is already acquired 
     height, width = shared_frame.frame.shape[:2]
     anchor_landmark = landmarks[anchor]
-    anchor_x = anchor_landmark.x * width
-    anchor_y = anchor_landmark.y * height
+    anchor_x = anchor_landmark.x
+    anchor_y = anchor_landmark.y
 
     for index in landmark_indexes: 
         landmark = landmarks[index]
-        x = int(landmark.x * width) 
-        y = int(landmark.y * height) 
+        x = landmark.x 
+        y = landmark.y
         feature_x = x - anchor_x 
         feature_y = y - anchor_y 
         output_target.append(feature_x)
         output_target.append(feature_y)
 
         if draw: 
-            cv2.circle(shared_frame.frame, (x,y), 5, 255)
+            cv2.circle(shared_frame.frame, (int(x * width),int(y * height)), 5, 255)
 
 def feature_extraction(shared_frame: SharedFrame): 
     # each time you call this function yields one single training example. 
@@ -81,7 +97,10 @@ def get_calibration_data(shared_frame: SharedFrame):
     # temp variables 
     training_features = [] 
     training_targets = [] 
-    num_features = len(right_eye_landmarks) + len(left_eye_landmarks) + len(nose_landmarks)
+
+    # num_features = num_landmarks * features_per_landmark 
+    # we have four features per landmark, the relative xy and the true xy 
+    num_features = (len(right_eye_landmarks) + len(left_eye_landmarks) + len(nose_landmarks)) * 2  
 
     # create black screen 
     screen = np.zeros((display_height, display_width, 3), dtype=np.uint8)
@@ -97,15 +116,16 @@ def get_calibration_data(shared_frame: SharedFrame):
     while True: 
         cv2.imshow("Calibration", screen)
 
-        # get mouse position 
-        mouse_x, mouse_y = pyautogui.position() 
+        if shared_frame.frame is not None: 
+            # get mouse position 
+            mouse_x, mouse_y = pyautogui.position() 
 
-        # get features 
-        features = feature_extraction(shared_frame)
+            # get features 
+            features = feature_extraction(shared_frame)
 
-        # store training example 
-        training_features.append(features)
-        training_targets.append((mouse_x, mouse_y))
+            # store training example 
+            training_features.append(features)
+            training_targets.append((mouse_x / display_width, mouse_y / display_height))
 
         key = cv2.waitKey(1) 
 
@@ -131,11 +151,28 @@ def get_calibration_data(shared_frame: SharedFrame):
 
     cv2.destroyAllWindows() 
 
+def save_model(model_obj):
+    name = unique_filename("models/regressor", ".pkl")
+    file = open(name, "wb")
+    pickle.dump(model_obj, file)
+    file.close() 
+    print(f"Successfully saved model to {name}")
+
+def load_model(filename): 
+    name = f"models/{filename}.pkl"
+    file = open(name, "rb")
+    model_obj = pickle.load(file) 
+    file.close() 
+    return model_obj
+
 def train(filename): 
     # each training example is a RGB photo of the eyes from face landmarker 
     data = pd.read_csv(filename)
     X = data.iloc[:, :-2].values 
     y = data.iloc[:, -2:].values 
+
+    print(f"Num Features: {len(X[0])}")
+    print(f"Num Target Values: {len(y[0])}")
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123)
 
@@ -146,17 +183,23 @@ def train(filename):
     base_regressor = MLPRegressor(
         hidden_layer_sizes=(64, 64,),
         random_state=123,
-        max_iter=2000
+        max_iter=2000, 
+        verbose=True
     )
     regressor = MultiOutputRegressor(base_regressor).fit(X_train_scaled, y_train)
     train_score = regressor.score(X_train_scaled, y_train)
     test_score = regressor.score(X_test_scaled, y_test) 
 
+    model = Model(regressor, scaler)
+
     print(train_score)
     print(test_score)
+    save_model(model)
 
-def predict(frame: SharedFrame): 
-    pass 
+def predict(shared_frame: SharedFrame, model: Model): 
+    if shared_frame.frame is not None: 
+        predictions = model.predict(shared_frame)
+        return predictions 
 
 
 
